424adf14e6af941c29de67ffe89bc7ff145405b0
fariz dzulhami <dzulhamipablo@gmail.com>
2026-05-09 13:15:35 +0700

    feat: CRUD Kategori Produk - migration, model, controller, views. Closes #4

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Kategori Produk
+Route::resource('categories', CategoryController::class)->except(['show']);
